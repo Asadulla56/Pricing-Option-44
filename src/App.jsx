24d94fabce10c0ab4661ba4ac43a35c621cb4824
Asadulla56 <mdasadullahoque294@gmail.com>
2024-03-20 01:48:14 +0600

    The first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navbar from "./compontents/Navbar/Navbar";
+
+function App() {
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <Navbar></Navbar>
+      
+    </>
+  );
+}
+
+export default App;
